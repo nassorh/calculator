c2094f7a61619ec7227276ac7c48b1b8bd3f3ace
nassorh <nassorh.dev@gmail.com>
2024-10-04 15:56:39 +0100

    Initial Commit

---
+++ calculator/calculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  calculatorApp.swift
+//  calculator
+//
+//  Created by Nassor, Hamad on 04/10/2024.
+//
+
+import SwiftUI
+
+@main
+struct calculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
