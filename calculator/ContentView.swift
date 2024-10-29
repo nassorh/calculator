ffbf91762d5eed682a9f117ff9646b96b0882b2e
nassorh <nassorh.dev@gmail.com>
2024-10-29 14:57:10 +0000

    Fix: Changed State to StateObject in order to observe published values

--- calculator/ContentView.swift
+++ calculator/ContentView.swift
@@ -8,7 +8,7 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State private var viewModel = ContentViewModel()
+    @StateObject private var viewModel = ContentViewModel()
     @Environment(\.horizontalSizeClass) var horizontalSizeClass
     
     var body: some View {
